4a3bfa37e0f4e5c4f8f31c38ec65e2fddf6a2c55
gosaimuk <gosaimuk@LIN-5CG134C0JX>
2023-10-23 16:14:40 +0530

    Add project files.

---
+++ Interest/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interest;
+
+Console.WriteLine("Hello, World!");
+SimpleInterest si = new SimpleInterest();
+si.Display();
---
+++ Interest/SimpleInterest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest
+{
+    internal class SimpleInterest
+    {
+        int p;
+        int t;
+        int r;
+        int i;
+        public void AcceptValue()
+        {
+            Console.WriteLine("Enter the Principle amount: ");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the time period: ");
+            t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the rate of intrest: ");
+            r = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Display()
+        {
+            AcceptValue();
+            i = (p * t * r) / 100;
+            Console.WriteLine("Simple-Intrest is : " + i);
+        }
+    }
+}
+
